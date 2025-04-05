3ea9b3334d9216b949a5c75eaec378a42842cbcb
LeonardoAValerio <leonardoaugustovalerio@gmail.com>
2025-04-04 22:21:38 -0300

    feat: create games on index

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,23 @@ import express from "express"
 import { config } from "dotenv"
 import { createServer } from "node:http";
 import { Server } from "socket.io";
+import { Game } from "./services/game/game";
 config();
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server, {
+export const io = new Server(server, {
     cors: {
         origin: "*"
     }
 });
 
+export const games: Record<string, Game> = {}
+
 const port = process.env.PORT || "8000";
-const games: string[] = [];
 
 server.listen(port, () => {
     console.log(`Server starter in port: ${port}`);
-});
-
-app.post("/createGame", () => {
-    io.of("/games/" + "1").on("connection", (socket) => {
-
-    })
 });
 
 
