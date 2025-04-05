83f4fecb53d33009ac09341bcf147fb05c43b954
LeonardoAValerio <leonardoaugustovalerio@gmail.com>
2025-04-04 22:21:04 -0300

    feat: create starter game

--- src/services/game/game.ts
+++ src/services/game/game.ts
@@ -1,8 +1,20 @@
+import { Map } from "../../models/map/map";
+import { NoiseMap } from "../../models/map/noiseMap";
+
 export interface GameAttributes {
     rows: number;
     columns: number;
 }
 
 export class Game {
-
+    private _map: Map;
+    
+    constructor(attributes: GameAttributes) {
+        this._map = new Map({noiseMap: new NoiseMap({
+            columns: attributes.columns,
+            rows: attributes.rows,
+            max: 5,
+            min: 0
+        })});
+    }
 }
